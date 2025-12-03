abc86248ad5b41fb50ede7f66c53149454d4dd64
Pasin Jinda <porh8pasin@gmail.com>
2025-12-03 16:53:01 +0700

    modified precision landing

--- src/aruco_land/src/landing_node.cpp
+++ src/aruco_land/src/landing_node.cpp
@@ -21,17 +21,22 @@ public:
         offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
         trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
         vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+        arm_timer_ = this->create_wall_timer(
+            1000ms, std::bind(&LandingNode::startOffboard, this)
+        );
 
         // Subscribers
         pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-            "/vision/aruco_pose", qos,
+            "/target_pose", qos,
             std::bind(&LandingNode::poseCallback, this, std::placeholders::_1)
         );
-
+        
+        /*
         mission_status_sub_ = this->create_subscription<std_msgs::msg::Bool>(
             "/mission/reached_final_waypoint", 10, 
             std::bind(&LandingNode::controlCallback, this, std::placeholders::_1)
         );
+        */
 
         // Main Control Loop Timer (Runs at 20Hz)
         timer_ = this->create_wall_timer(50ms, std::bind(&LandingNode::timerCallback, this));
@@ -53,6 +58,7 @@ private:
     rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
     rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
     rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
+    rclcpp::TimerBase::SharedPtr arm_timer_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     // Receive detection from Vision Node
@@ -64,32 +70,16 @@ private:
         // Vision Coordinate (Camera Frame)
         // Usually for Down Facing Camera: X=Right, Y=Down(Forward in image), Z=Depth
         // We need to map this to Drone Body Frame: X=Forward, Y=Right, Z=Down
-        
         // Save the error to use in the timer loop
         err_x = msg->pose.position.x; 
         err_y = msg->pose.position.y;
         err_z = msg->pose.position.z;
     }
 
-    void controlCallback(const std_msgs::msg::Bool::SharedPtr msg)
-    {
-        if (msg->data) {
-            mission_node_stopped = true;
-            RCLCPP_INFO(this->get_logger(), "Mission finished. Landing Node taking control!");
-        }
-    }
-
     void timerCallback()
     {
-        // 1. Only run if mission node is done
-        if (!mission_node_stopped) {
-            return; 
-        }
-
-        // 2. Publish Offboard Control Mode (MUST stream constantly)
         publish_offboard_control_mode();
 
-        // 3. Check if we recently saw the marker
         bool is_valid_detection = false;
         
         if (marker_detected_) {
@@ -140,7 +130,7 @@ private:
             RCLCPP_INFO(get_logger(), "Aligning: Vx=%.2f Vy=%.2f Dist=%.2f", vel_x, vel_y, err_z);
 
             // Landing Trigger
-            if (err_z < 0.3) { 
+            if (err_z < 0.6) { 
                 RCLCPP_INFO(get_logger(), "Close enough! Triggering LAND mode.");
                 this->publish_vehicle_command(VehicleCommand::VEHICLE_CMD_NAV_LAND);
                 timer_->cancel(); 
@@ -159,7 +149,7 @@ private:
         msg.attitude = false;
         msg.body_rate = false;
         msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-        offboard_control_mode_publisher_->publish(msg);
+        offboard_control_mode_publisher_->publish(msg);     
     }
 
     void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0) {
@@ -175,6 +165,19 @@ private:
         msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
         vehicle_command_publisher_->publish(msg);
     }
+
+    void startOffboard()
+    {
+        RCLCPP_INFO(this->get_logger(), "Sending ARM + OFFBOARD commands...");
+
+        // 1. Arm
+        publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+
+        // 2. Request OFFBOARD
+        publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1.0, 6.0);
+        // px4 mode 6 = offboard
+    }
+
 };
 
 int main(int argc, char **argv)
